a5853a3cceae45e86b614b6205391bff4f7725de
kanuoli11 <strubel.oliver@googlemail.com>
2026-07-05 21:17:21 +0200

    Update maps.py

--- modules/maps.py
+++ modules/maps.py
@@ -1,29 +1,30 @@
 import folium
-from streamlit_folium import st_folium
-
 
 def create_base_map():
-    """
-    Zentrale Deutschland-Karte
-    """
     return folium.Map(
         location=[51.2, 10.5],
         zoom_start=6,
         tiles="OpenStreetMap"
     )
 
-
 def add_dummy_points(m):
-    """
-    Platzhalterpunkte (später echte Geodaten)
-    """
-    folium.Marker(
-        [51.5, 7.4],
+    folium.CircleMarker(
+        location=[51.5, 7.4],
+        radius=10,
+        color="#0d6efd",
+        fill=True,
+        fill_color="#0d6efd",
+        fill_opacity=0.9,
         tooltip="Beispiel: NRW Pegel"
     ).add_to(m)
 
-    folium.Marker(
-        [48.1, 11.5],
+    folium.CircleMarker(
+        location=[48.1, 11.5],
+        radius=10,
+        color="#d62828",
+        fill=True,
+        fill_color="#d62828",
+        fill_opacity=0.9,
         tooltip="Beispiel: Bayern Sperrung"
     ).add_to(m)
 
