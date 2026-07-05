54353b639bbe8a3fdfb150c8117ca82ac7b19ed7
kanuoli11 <strubel.oliver@googlemail.com>
2026-07-05 16:18:01 +0200

    sperrungen logic extracted from app.py

--- modules/sperrungen.py
+++ modules/sperrungen.py
@@ -1,1 +1,45 @@
-initial structure
+from datetime import datetime, date
+import pandas as pd
+
+def split_sperrungen(sp_list):
+    """
+    Trennt Sperrungen in aktiv und archiviert
+    """
+    heute = date.today()
+
+    aktiv = []
+    archiv = []
+
+    for s in sp_list:
+        try:
+            gueltig_bis = datetime.strptime(s["Gültig_bis"], "%Y-%m-%d").date()
+
+            if gueltig_bis >= heute:
+                aktiv.append(s)
+            else:
+                archiv.append(s)
+
+        except:
+            # Falls Datum kaputt ist -> sicherheitshalber aktiv anzeigen
+            aktiv.append(s)
+
+    return aktiv, archiv
+
+
+def filter_sperrungen(sp_list, bundesland=None, typ_filter=None):
+    """
+    Filtert Sperrungen nach Bundesland und Typ
+    """
+
+    df = pd.DataFrame(sp_list)
+
+    if df.empty:
+        return df
+
+    if bundesland and bundesland != "Alle":
+        df = df[df["Bundesland"] == bundesland]
+
+    if typ_filter:
+        df = df[df["Typ"].isin(typ_filter)]
+
+    return df
